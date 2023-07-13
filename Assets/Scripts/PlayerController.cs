9878e0f64240ded2dbf2ec778a913be1b64ce267
JacovHemert <106889415+JacovHemert@users.noreply.github.com>
2023-07-14 00:55:18 +0200

    Added controls for player and modified tilemap

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,27 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
-
+    private Vector2 desiredMove;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
+
     }
+
+    public void PrintStuff()
+    {
+        Debug.Log("PRASDFADS");
+    }
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log(desiredMove);
+        desiredMove = context.ReadValue<Vector2>();
+        
+    }
+
 
     private void Update()
     {
-
+        
         
 
     }
 
     private void FixedUpdate()
     {
-        
+        rb.MovePosition(new Vector2(transform.position.x + desiredMove.x, transform.position.y + desiredMove.y));
+
+
     }
 
 }
