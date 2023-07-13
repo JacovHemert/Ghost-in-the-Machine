8e0b4c89ee88161ccc9eab9cc21b05432e80b127
JacovHemert <106889415+JacovHemert@users.noreply.github.com>
2023-07-13 21:57:08 +0200

    Added tileset and tested

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+}
