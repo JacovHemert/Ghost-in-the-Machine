038ed4f2072570fd68cb9e1460af08f0a10b5487
dlwalker <danieltrwalker@gmail.com>
2023-07-24 17:50:41 -0700

    Fixed audio bug on game load
    stopped the sample audio from playing immediately when the main menu loads

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -17,12 +17,14 @@ public class MainMenuController : MonoBehaviour
     private const string SFXVolumeParam = "SFX Volume";
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         settingMenu.SetActive(false);
 
         musicSlider.value = GetMusicVolume();
         sfxSlider.value = GetSFXVolume();
+
+        sfxSlider.onValueChanged.AddListener(OnSFXVolumeChanged);
     }
 
     public void OnStartButtonClick()
